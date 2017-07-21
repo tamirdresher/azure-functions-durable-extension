aaf81787f801bc0554fd08e3ad8562be31ffd4ee
tamir.dresher@gmail.com <tamir.dresher@gmail.com>
2017-07-21 23:04:51 +0300

    HelloWorld and Auction

--- samples/precompiled/HelloSequence.cs
+++ samples/precompiled/HelloSequence.cs
@@ -1,34 +1,60 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
+
 
 namespace VSSample
 {
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Azure.WebJobs.Host;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
     public static class HelloSequence
     {
-        [FunctionName("E1_HelloSequence")]
-        public static async Task<List<string>> Run(
-            [OrchestrationTrigger] DurableOrchestrationContext context)
+        [FunctionName("HelloWorldExecutor")]
+        public static async Task<HttpResponseMessage> ExecuteHelloWorld(
+            [HttpTrigger(AuthorizationLevel.Anonymous, methods: "post", Route = "HelloWorld")] HttpRequestMessage req,
+            [OrchestrationClient] DurableOrchestrationClient client,
+            TraceWriter log)
+        {
+            object functionInput = null;
+            string instanceId = await client.StartNewAsync(nameof(HelloWorld), functionInput);
+
+            //return req.CreateResponse(HttpStatusCode.OK, $"New HelloWorld instance created with ID = '{instanceId}'");
+
+            return client.CreateCheckStatusResponse(req, instanceId);
+        }
+
+
+        [FunctionName("HelloWorld")]
+        public static async Task<List<string>> HelloWorld(
+            [OrchestrationTrigger] DurableOrchestrationContext context,
+            TraceWriter log)
         {
             var outputs = new List<string>();
 
-            outputs.Add(await context.CallFunctionAsync<string>("E1_SayHello", "Tokyo"));
-            outputs.Add(await context.CallFunctionAsync<string>("E1_SayHello", "Seattle"));
-            outputs.Add(await context.CallFunctionAsync<string>("E1_SayHello", "London"));
+            outputs.Add(await context.CallFunctionAsync<string>("Say", "Hello"));
+            outputs.Add(await context.CallFunctionAsync<string>("Say", " "));
+            outputs.Add(await context.CallFunctionAsync<string>("Say", "World!"));
 
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            log.Info(string.Concat(outputs));
+
+            // returns ["Hello", "World"]
             return outputs;
         }
 
-        [FunctionName("E1_SayHello")]
-        public static string SayHello(
-            [ActivityTrigger] DurableActivityContext helloContext)
+        [FunctionName("Say")]
+        public static string Say(
+            [ActivityTrigger] DurableActivityContext activityContext,
+            TraceWriter log)
         {
-            string name = helloContext.GetInput<string>();
-            return $"Hello {name}!";
+            string word = activityContext.GetInput<string>();
+            log.Info(word);
+            return word;
         }
     }
- }
+}
---
+++ samples/precompiled/RunAuction.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VSSample
+{
+
+    public static class RunAuction
+    {
+        [FunctionName("AddAsk")]
+        public static async Task<List<BidInfo>> Run(
+            [OrchestrationTrigger] DurableOrchestrationContext context,
+            TraceWriter log)
+        {
+            var product = context.GetInput<string>();
+            var outputs = new List<BidInfo>();
+            log.Info("calling SaveAsk");
+            var askInfo = await context.CallFunctionAsync<AskInfo>("SaveAsk", product);
+            log.Info($"AskInfo - Id: {askInfo.AskId}");
+
+            using (var timeoutCts = new CancellationTokenSource())
+            {
+                DateTime expiration = context.CurrentUtcDateTime.AddSeconds(10);
+                await context.CreateTimer(expiration, timeoutCts.Token);
+                log.Info("delay done");
+
+                var highestBid = await context.CallFunctionAsync<BidInfo>("GetHighestBid", askInfo.AskId);
+                log.Info($"highestBid - Id: {highestBid.BidId} AskInfo.Id: {askInfo?.AskId}");
+
+                outputs.Add(highestBid);
+
+                // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+                return outputs;
+            }
+        }
+
+        [FunctionName("SaveAsk")]
+        public static AskInfo SaveAsk(
+            [ActivityTrigger] DurableActivityContext helloContext)
+        {
+            string productname;
+            try
+            {
+                productname = helloContext.GetInput<string>();
+
+            }
+            catch (Exception)
+            {
+
+                productname = "problem with GetInput";
+            }
+            //return $"Saving {name}!";
+
+            return new AskInfo
+            {
+                AskId = Guid.NewGuid(),
+                ProductName = productname
+            };
+        }
+
+        [FunctionName("GetHighestBid")]
+        public static BidInfo GetHighestBid(
+           [ActivityTrigger] DurableActivityContext helloContext)
+        {
+            Guid askId = helloContext.GetInput<Guid>();
+            //return $"Saving {name}!";
+
+            return new BidInfo
+            {
+                BidId = Guid.NewGuid(),
+                AskId = askId,
+
+                UserName = "Tamir",
+                UserEmail = "Tamir@Dresher.com",
+                Price = 100
+            };
+        }
+    }
+
+
+    public class AskInfo
+    {
+        public AskInfo()
+        {
+            Console.WriteLine("AskInfo Created");
+        }
+        public Guid AskId { get; set; }
+        public string ProductName { get; set; }
+    }
+
+    public class BidInfo
+    {
+        public Guid BidId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+
+        public Guid AskId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
